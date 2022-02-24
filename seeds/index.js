02334a773ac7c7cc50f91c6040b8dbc2251cd3f0
Kieran Harris <kharris19063@gmail.com>
2022-02-24 02:55:15 -0500

    Cleaning up code, edited files names to accurately reflect contents

--- seeds/index.js
+++ seeds/index.js
@@ -1,21 +1,21 @@
 const sequelize = require('../config/connection');
-const seedComments = require("./commentSeed");
-const seedUsers = require("./userSeed");
-const seedPosts = require("./postSeed");
+const seedComments = require("./commentData");
+const seedUsers = require("./userData");
+const seedPosts = require("./postData");
 
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
   console.log("DATABASE SYNCED");
 
-  await seedComments();
-  console.log("DATABASE SYNCED");
-
   await seedUsers();
-  console.log("DATABASE SYNCED");
+  console.log("\n---- USERS SEED SUCCESSFUL ----\n");
 
   await seedPosts();
-  console.log("DATABASE SYNCED");
+  console.log("\n---- POSTS SEED SUCCESSFUL ----\n");
+
+  await seedComments();
+  console.log("\n---- COMMENTS SEED SUCCESSFUL ----\n");
 
   process.exit(0);
 };
